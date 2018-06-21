ab758afce69216eee4f6aef73680a56d9590a739
Zak-JS <zakelas@gmail.com>
2018-06-21 16:24:47 +0100

    New calculated additional fill functionality
    Allows a user to enter a additional fill value and color to represent multiple values instead of just 1

--- src/CircularProgress.js
+++ src/CircularProgress.js
@@ -1,97 +1,105 @@
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import { View, ViewPropTypes, Platform, ART, AppState } from 'react-native';
-const { Surface, Shape, Path, Group } = ART;
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, Platform, ViewPropTypes } from 'react-native'
+import {
+  Surface,
+  Shape,
+  Path,
+  Group
+} from '../../react-native/Libraries/ART/ReactNativeART'
+import MetricsPath from 'art/metrics/path'
 
 export default class CircularProgress extends React.Component {
-
-  state = {
-    // We need to track this to mitigate a bug with RN ART on Android.
-    // After being unlocked the <Surface> is not rendered.
-    // To mitigate this we change the key-prop to forcefully update the <Surface>
-    // It's horrible.
-    // See https://github.com/facebook/react-native/issues/17565
-    appState: AppState.currentState,
-  }
-
   circlePath(cx, cy, r, startDegree, endDegree) {
-    let p = Path();
-    p.path.push(0, cx + r, cy);
-    p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, (endDegree * .9999) * Math.PI / 180, 1);
-    return p;
+    const p = Path()
+    p.path.push(0, cx + r, cy)
+    p.path.push(
+      4,
+      cx,
+      cy,
+      r,
+      startDegree * Math.PI / 180,
+      endDegree * Math.PI / 180,
+      1
+    )
+    return p
   }
 
-  clampFill = fill => Math.min(100, Math.max(0, fill));
+  extractFill(fill) {
+    if (fill < 0.01) {
+      return 0
+    } else if (fill > 100) {
+      return 100
+    }
 
-  componentDidMount = () => AppState.addEventListener('change', this.handleAppStateChange);
-  
-  componentWillUnmount = () => AppState.removeEventListener('change', this.handleAppStateChange);
-
-  handleAppStateChange = appState => this.setState({ appState });
+    return fill
+  }
 
   render() {
     const {
       size,
       width,
-      backgroundWidth,
       tintColor,
       backgroundColor,
       style,
       rotation,
-      lineCap,
-      arcSweepAngle,
-      renderChild,
-      fill,
-    } = this.props;
+      linecap,
+      additionalFillTintColor,
+      children
+    } = this.props
+    const backgroundPath = this.circlePath(
+      size / 2,
+      size / 2,
+      size / 2 - width / 2,
+      0,
+      360 * 0.9999
+    )
 
-    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, arcSweepAngle);
-    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, arcSweepAngle * this.clampFill(fill) / 100);
-    const offset = size - (width * 2);
+    const fill = this.extractFill(this.props.fill)
+    const additionalFill = this.extractFill(this.props.additionalFill)
 
-    const childContainerStyle = {
-      position: 'absolute',
-      left: width,
-      top: width,
-      width: offset,
-      height: offset,
-      borderRadius: offset / 2,
-      alignItems: 'center',
-      justifyContent: 'center'
-    };
+    const circlePath = this.circlePath(
+      size / 2,
+      size / 2,
+      size / 2 - width / 2,
+      0,
+      360 * 0.9999 * fill / 100
+    )
+
+    const additionalFillCirclePath = this.circlePath(
+      size / 2,
+      size / 2,
+      size / 2 - width / 2,
+      0,
+      360 * 0.9999 * additionalFill / 100 + 360 * 0.9999 * fill / 100
+    )
 
     return (
       <View style={style}>
-        <Surface
-          width={size}
-          height={size}
-          key={this.state.appState}
-          style={{ backgroundColor: 'transparent' }}
-        >
-          <Group rotation={rotation - 90} originX={size/2} originY={size/2}>
-            { backgroundColor && (
-              <Shape
-                d={backgroundPath}
-                stroke={backgroundColor}
-                strokeWidth={backgroundWidth || width}
-                strokeCap={lineCap}
-              />
-            )}
+        <Surface width={size} height={size}>
+          <Group rotation={rotation - 90} originX={size / 2} originY={size / 2}>
+            <Shape
+              d={backgroundPath}
+              stroke={backgroundColor}
+              strokeWidth={width}
+            />
+            <Shape
+              d={additionalFillCirclePath}
+              stroke={additionalFillTintColor}
+              strokeWidth={width}
+              strokeCap={linecap}
+            />
             <Shape
               d={circlePath}
               stroke={tintColor}
               strokeWidth={width}
-              strokeCap={lineCap}
+              strokeCap={linecap}
             />
           </Group>
         </Surface>
-        {renderChild && (
-          <View style={childContainerStyle}>
-            {renderChild(fill)}
-          </View>
-        )}
+        {children && children(fill)}
       </View>
-    );
+    )
   }
 }
 
@@ -100,18 +108,17 @@ CircularProgress.propTypes = {
   size: PropTypes.number.isRequired,
   fill: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
-  backgroundWidth: PropTypes.number,
   tintColor: PropTypes.string,
   backgroundColor: PropTypes.string,
   rotation: PropTypes.number,
-  lineCap: PropTypes.string,
-  arcSweepAngle: PropTypes.number,
-  renderChild: PropTypes.func
-};
+  linecap: PropTypes.string,
+  children: PropTypes.func
+}
 
 CircularProgress.defaultProps = {
   tintColor: 'black',
+  additionalFillTintColor: 'black',
+  backgroundColor: '#e4e4e4',
   rotation: 90,
-  lineCap: 'butt',
-  arcSweepAngle: 360
-};
+  linecap: 'butt'
+}
